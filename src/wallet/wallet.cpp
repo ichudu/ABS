a4a1768d133003f6249aa58dbeda7686b21dbc51
Wladimir J. van der Laan <laanwj@gmail.com>
2016-10-21 15:30:14 +0200

    Merge #8990: moveonly: move `coincontrol` to `src/wallet`
    1ae5839 moveonly: move `coincontrol` to `src/wallet` (Wladimir J. van der Laan)

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -10,7 +10,7 @@
 #include "base58.h"
 #include "checkpoints.h"
 #include "chain.h"
-#include "coincontrol.h"
+#include "wallet/coincontrol.h"
 #include "consensus/consensus.h"
 #include "consensus/validation.h"
 #include "key.h"
