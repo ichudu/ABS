c849add1f7ff51f2f98f7ba6fe4ea6a155acb870
Wladimir J. van der Laan <laanwj@gmail.com>
2016-11-02 11:12:48 +0100

    Merge #8448: Store mempool and prioritization data to disk
    582068a Add mempool.dat to doc/files.md (Pieter Wuille)
    3f78562 Add DumpMempool and LoadMempool (Pieter Wuille)
    ced7c94 Add AcceptToMemoryPoolWithTime function (Pieter Wuille)
    c3efb58 Add feedelta to TxMempoolInfo (Pieter Wuille)

--- src/init.cpp
+++ src/init.cpp
@@ -250,6 +250,7 @@ void PrepareShutdown()
     flatdb4.Dump(netfulfilledman);
 
     UnregisterNodeSignals(GetNodeSignals());
+    DumpMempool();
 
     if (fFeeEstimatesInitialized)
     {
@@ -759,6 +760,8 @@ void ThreadImport(std::vector<boost::filesystem::path> vImportFiles)
         LogPrintf("Stopping after block import\n");
         StartShutdown();
     }
+
+    LoadMempool();
 }
 
 /** Sanity checks
--- src/txmempool.h
+++ src/txmempool.h
@@ -326,6 +326,9 @@ struct TxMempoolInfo
 
     /** Feerate of the transaction. */
     CFeeRate feeRate;
+
+    /** The fee delta. */
+    int64_t nFeeDelta;
 };
 
 class SaltedTxidHasher
