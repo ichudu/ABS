4c37230be262e0a73d599edd83a3cf4ed352e4f1
Wladimir J. van der Laan <laanwj@gmail.com>
2016-11-03 10:19:59 +0100

    Merge #9071: Declare wallet.h functions inline
    2ca882a Declare wallet.h functions inline (Pieter Wuille)

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -174,7 +174,7 @@ struct CRecipient
 typedef std::map<std::string, std::string> mapValue_t;
 
 
-static void ReadOrderPos(int64_t& nOrderPos, mapValue_t& mapValue)
+static inline void ReadOrderPos(int64_t& nOrderPos, mapValue_t& mapValue)
 {
     if (!mapValue.count("n"))
     {
@@ -185,7 +185,7 @@ static void ReadOrderPos(int64_t& nOrderPos, mapValue_t& mapValue)
 }
 
 
-static void WriteOrderPos(const int64_t& nOrderPos, mapValue_t& mapValue)
+static inline void WriteOrderPos(const int64_t& nOrderPos, mapValue_t& mapValue)
 {
     if (nOrderPos == -1)
         return;
