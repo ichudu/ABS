7d96373eb3db927fa8fd37ec2251cc0e13295cc4
Wladimir J. van der Laan <laanwj@gmail.com>
2016-12-06 15:41:15 +0100

    Merge #9296: Fix missed change to WalletTx structure
    28f8ae8 Fix missed change to WalletTx structure (Alex Morcos)

--- src/qt/walletmodel.cpp
+++ src/qt/walletmodel.cpp
@@ -388,9 +388,8 @@ WalletModel::SendCoinsReturn WalletModel::sendCoins(WalletModelTransaction &tran
         if(!wallet->CommitTransaction(*newTx, *keyChange, g_connman.get(), recipients[0].fUseInstantSend ? NetMsgType::TXLOCKREQUEST : NetMsgType::TX))
             return TransactionCommitFailed;
 
-        CTransaction* t = (CTransaction*)newTx;
         CDataStream ssTx(SER_NETWORK, PROTOCOL_VERSION);
-        ssTx << *t;
+        ssTx << *newTx->tx;
         transaction_array.append(&(ssTx[0]), ssTx.size());
     }
 
