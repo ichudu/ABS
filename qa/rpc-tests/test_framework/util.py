54a770659e4117f733543e68dd7bb2ff909d69b0
Alexander Block <ablock84@gmail.com>
2018-01-31 10:40:27 +0100

    Explicitly start nodes with -usehd=1 in wallet-dump.py
    We currently have usehd defaulted to 0, so we need to explicitly start it
    with usehd=1 in wallet-dump.py.
    
    This requires setting up a new data dirs cache for hd as the wallets won't
    be compatible otherwise. At the same time we need the initial state of
    the wallet to test the dump functionality.
    
    Also use redirect_stderr=True to fix failure when run from pull-tester

--- qa/rpc-tests/test_framework/util.py
+++ qa/rpc-tests/test_framework/util.py
@@ -239,7 +239,7 @@ def wait_for_bitcoind_start(process, url, i):
                 raise # unknown JSON RPC exception
         time.sleep(0.25)
 
-def initialize_chain(test_dir, num_nodes, cachedir):
+def initialize_chain(test_dir, num_nodes, cachedir, extra_args=None):
     """
     Create a cache of a 200-block-long chain (with wallet) for MAX_NODES
     Afterward, create num_nodes copies from the cache
@@ -265,6 +265,8 @@ def initialize_chain(test_dir, num_nodes, cachedir):
             args = [ os.getenv("ABSOLUTED", "absoluted"), "-server", "-keypool=1", "-datadir="+datadir, "-discover=0" ]
             if i > 0:
                 args.append("-connect=127.0.0.1:"+str(p2p_port(0)))
+            if extra_args is not None:
+                args += extra_args
             bitcoind_processes[i] = subprocess.Popen(args)
             if os.getenv("PYTHON_DEBUG", ""):
                 print("initialize_chain: absoluted started, waiting for RPC to come up")
