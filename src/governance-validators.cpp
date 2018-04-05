9a099888a85d473eccd02e2a62d44688292d5e89
UdjinM6 <UdjinM6@users.noreply.github.com>
2018-04-05 15:34:26 +0300

    Validate data size for proposals only (#2004)
    There are no watchdogs and it makes no sense to limit triggers by size (invalid triggers are going to be voted down anyway).
    512 bytes should be more than enough for any proposal - the only value we do not check size of is URL and with 512 bytes total limit URL has space for almost 300 bytes.

--- src/governance-validators.cpp
+++ src/governance-validators.cpp
@@ -12,6 +12,7 @@
 
 #include <algorithm>
 
+const size_t MAX_DATA_SIZE  = 512;
 const size_t MAX_NAME_SIZE  = 40;
 
 CProposalValidator::CProposalValidator(const std::string& strHexData) :
@@ -27,6 +28,10 @@ CProposalValidator::CProposalValidator(const std::string& strHexData) :
 void CProposalValidator::ParseStrHexData(const std::string& strHexData)
 {
     std::vector<unsigned char> v = ParseHex(strHexData);
+    if (v.size() > MAX_DATA_SIZE) {
+        strErrorMessages = strprintf("data exceeds %lu characters;", MAX_DATA_SIZE);
+        return;
+    }
     ParseJSONData(std::string(v.begin(), v.end()));
 }
 
