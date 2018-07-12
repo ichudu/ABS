37cc6698780955987ac954c9e2b7594072a4b7ae
PaulieD <jedipbb1383@gmail.com>
2018-07-12 04:06:30 -0500

    Implement Governance ZMQ notification messages (#2160)
    * fix whitespace
    
    * added zmq stuff for governance objects and votes
    it seems that zmq is currently not in a working state, need to figure that out.
    
    Need to:
    plug in the new methods added
    possibly plug in the old methods, as it doesn't look like they are.
    
    * formatting fix. Will probably need to revert for this PR
    
    * continue linking new zmq messages in
    
    * added comment, might need to revert
    
    * fixes error of it not knowing about the classes
    
    * Actually link in, all new govobjects and govvotes should be broadcast over zmq now.
    
    * fix compile error, forgot to change params when copying
    
    * fix compile error
    
    * add imports to the header files in zmqconfig.h
    
    * fixing linking(i think) error
    
    * Revert "added comment, might need to revert"
    
    This reverts commit 2918ea40fe9a96834c4bd89e13cb458cde6814f2.
    
    * Revert "formatting fix. Will probably need to revert for this PR"
    
    This reverts commit ca10558866ab61e3dd0c70541fdcfee6f5115157.
    
    * fix tabs etc
    
    * EOL added
    
    * optimization of hash.begin() @nmarley thoughts?
    
    * remove formatting changes
    
    * iterator i -> it and removal of notifier
    
    * typo in df879f57
    
    * use auto for the iterators
    
    * introduce hash prefix
    
    * implement changes in zmq_sub.py, update the doc, and change argument name to fix typo
    
    * deref iterators before calling methods
    
    * continued e8a4c505
    
    * missed one... continued e8a4c505
    
    * killing some tabs
    
    * fix spacing for setting or comparing iterators
    
    * change order of new variables to match current setup
    
    * re-add elif's I didn't realize got removed
    
    * Revert "fix spacing for setting or comparing iterators"
    
    This reverts commit 8ce2068148dcd275ebba7ee6038d0db1c582b9f3.
    
    * Revert "use auto for the iterators"
    
    This reverts commit cb16cf0760bfaf68c56684877898611802bf2303.
    
    * Revert "missed one... continued e8a4c505"
    
    This reverts commit 2087cf894f7e9682508b4692b89897b4fa4e4b7a.
    
    * Revert "continued e8a4c505"
    
    This reverts commit a78c8ad2c9bb1602242a8f040e17ef958982348c.
    
    * Revert "deref iterators before calling methods"
    
    This reverts commit e8a4c505d1d34360eaf882d92cd8fbc55436cfcc.
    
    * Revert "iterator i -> it and removal of notifier"
    
    This reverts commit 29574248b1a0d05c18d60454d07c77979aae6fb2.
    
    * Revert "fix whitespace"
    
    This reverts commit 612be48d963000b4cbc5f64981a88a3225428b37.
    
    * Revert "typo in df879f5"
    
    * Revert "Optimization of hash.begin()"
    
    * fixes problem with revert
    
    * Udjin complain's a lot ;)
    
    * help text, init.cpp
    
    * add signals in validationinterface.cpp
    
    * Change location of vote notification call.
    
    * remain consistent
    
    * remove unneeded include due to change of notification location
    
    * implement raw notifications

--- src/governance.cpp
+++ src/governance.cpp
@@ -4,6 +4,7 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include "consensus/validation.h"
+#include "validationinterface.h"
 #include "governance.h"
 #include "governance-object.h"
 #include "governance-validators.h"
@@ -266,7 +267,8 @@ void CGovernanceManager::ProcessMessage(CNode* pfrom, const std::string& strComm
             }
             return;
         }
-
+        // SEND NOTIFICATION TO SCRIPT/ZMQ
+        GetMainSignals().NotifyGovernanceVote(vote);
     }
 }
 
@@ -362,6 +364,10 @@ void CGovernanceManager::AddGovernanceObject(CGovernanceObject& govobj, CConnman
 
     CGovernanceException exception;
     CheckOrphanVotes(govobj, exception, connman);
+
+    // SEND NOTIFICATION TO SCRIPT/ZMQ
+    GetMainSignals().NotifyGovernanceObject(govobj);
+
 
     DBG( std::cout << "CGovernanceManager::AddGovernanceObject END" << std::endl; );
 }
