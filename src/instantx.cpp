05954119f79855b36c6991396d0f399818e1ba3c
gladcow <sergey@dash.org>
2018-07-28 19:39:37 +0300

    Remove dummy confirmations in RPC API and GUI for InstantSend
    * remove instandsenddepth option
    
    * remove CInstantSend::GetConfirmations
    
    * Explicit IXlocks processing in the code using GetDepthInMainChain
    
    * remove dummy confirmations for IX locks
    
    * remove unused constant
    
    * Track IS status in TransactionStatus
    
    * Highlight IS in tx list
    
    * Customize confirmation icons for IS txes
    
    * Fix issues:
    
    - bring back removed comment;
    - simplify 2 complex condition to ose `IsTrusted()` function only;
    - rename `IsLockedIX` function to 'IsLockedByInstantSend`;
    
    * Revert condition with IsTrusted()
    
    * code style fix
    
    * remove dummy confirmations from instantsend.md
    
    * fix instantsend.md

--- src/instantx.cpp
+++ src/instantx.cpp
@@ -34,7 +34,6 @@ extern CWallet* pwalletMain;
 extern CTxMemPool mempool;
 
 bool fEnableInstantSend = true;
-int nInstantSendDepth = DEFAULT_INSTANTSEND_DEPTH;
 int nCompleteTXLocks;
 
 CInstantSend instantsend;
@@ -837,11 +836,6 @@ int CInstantSend::GetTransactionLockSignatures(const uint256& txHash)
     }
 
     return -1;
-}
-
-int CInstantSend::GetConfirmations(const uint256 &nTXHash)
-{
-    return IsLockedInstantSendTransaction(nTXHash) ? nInstantSendDepth : 0;
 }
 
 bool CInstantSend::IsTxLockCandidateTimedOut(const uint256& txHash)
