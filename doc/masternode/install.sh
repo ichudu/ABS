fedfd05dbae0f97e81ed629b449021035c4abebb
Crypto Centric <38781794+CryptoCentric@users.noreply.github.com>
2018-08-01 13:14:40 +0100

    Dependency update

--- doc/masternode/install.sh
+++ doc/masternode/install.sh
@@ -6,6 +6,7 @@ sudo apt-get install build-essential libtool autotools-dev automake pkg-config l
 sudo apt-get install libboost-all-dev -y
 sudo add-apt-repository ppa:bitcoin/bitcoin -y
 sudo apt-get install libzmq3-dev -y
+sudo apt-get install libminiupnpc-dev -y
 sudo apt-get update -y
 sudo apt-get install libdb4.8-dev libdb4.8++-dev -y
 sudo apt-get install libminiupnpc-dev -y
