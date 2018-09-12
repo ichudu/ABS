3c93dd31c9c597acdfcda4c92701410ce8e8f14b
Alexander Block <ablock84@gmail.com>
2018-09-12 13:13:17 +0200

    Call InitializeCurrentBlockTip and activeMasternodeManager
    While importing, IsInitialBlockDownload always returns true, meaning that
    the call to UpdatedBlockTip will also receive true as fInitialDownload.
    
    UpdatedBlockTip is never called again unless a block is mined.
    Mining of new blocks in regtest however only starts after mnsync has
    finished, which will never happen as it waits for the next call to
    UpdatedBlockTip.

--- src/init.cpp
+++ src/init.cpp
@@ -797,6 +797,15 @@ void ThreadImport(std::vector<boost::filesystem::path> vImportFiles)
         StartShutdown();
     }
     } // End scope of CImportingNow
+
+    // force UpdatedBlockTip to initialize nCachedBlockHeight for DS, MN payments and budgets
+    // but don't call it directly to prevent triggering of other listeners like zmq etc.
+    // GetMainSignals().UpdatedBlockTip(chainActive.Tip());
+    pdsNotificationInterface->InitializeCurrentBlockTip();
+
+    if (activeMasternodeManager && fAIP0003ActiveAtTip)
+        activeMasternodeManager->Init();
+
     LoadMempool();
     fDumpMempoolLater = !fRequestShutdown;
 }
@@ -1836,7 +1845,7 @@ bool AppInitMain(boost::thread_group& threadGroup, CScheduler& scheduler)
     }
 
     if((!fLiteMode && fTxIndex == false)
-       && chainparams.NetworkIDString() != CBaseChainParams::REGTEST) { 
+       && chainparams.NetworkIDString() != CBaseChainParams::REGTEST) {
         return InitError(_("Transaction index can't be disabled in full mode. Either start with -litemode command line switch or enable transaction index."));
     }
 
@@ -1972,18 +1981,7 @@ bool AppInitMain(boost::thread_group& threadGroup, CScheduler& scheduler)
         }
     }
 
-
-    // ********************************************************* Step 11c: update block tip in Absolute modules
-
-    // force UpdatedBlockTip to initialize nCachedBlockHeight for DS, MN payments and budgets
-    // but don't call it directly to prevent triggering of other listeners like zmq etc.
-    // GetMainSignals().UpdatedBlockTip(chainActive.Tip());
-    pdsNotificationInterface->InitializeCurrentBlockTip();
-
-    if (activeMasternodeManager && fAIP0003ActiveAtTip)
-        activeMasternodeManager->Init();
-
-    // ********************************************************* Step 11d: schedule Absolute-specific tasks
+    // ********************************************************* Step 11c: schedule Dash-specific tasks
 
     if (!fLiteMode) {
         scheduler.scheduleEvery(boost::bind(&CNetFulfilledRequestManager::DoMaintenance, boost::ref(netfulfilledman)), 60);
