73e86a82d1cf3c85c4249d0dfcc81e239d2e5271
CryptoCentric <contact@absolutecoin.net>
2018-12-18 19:57:02 +0000

    De-neuter Node Bloom
    Always disconnect old nodes which request filtered connections. (Patrick Strateman)

--- src/validation.h
+++ src/validation.h
@@ -138,7 +138,6 @@ static const bool DEFAULT_ENABLE_REPLACEMENT = false;
 static const unsigned int MAX_BLOCKS_TO_ANNOUNCE = 8;
 
 static const bool DEFAULT_PEERBLOOMFILTERS = true;
-static const bool DEFAULT_ENFORCENODEBLOOM = false;
 struct BlockHasher
 {
     size_t operator()(const uint256& hash) const { return hash.GetCheapHash(); }
