f84c565818e92ef6eb363f4ddd6fcd7691294f76
CryptoCentric <contact@absolutecoin.net>
2018-12-18 23:11:52 +0000

    CWallet* pwalletMain Update
    CWallet* pwalletMain Update

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -44,6 +44,7 @@
 
 using namespace std;
 
+CWallet* pwalletMain = NULL;
 /** Transaction fee set by the user */
 CFeeRate payTxFee(DEFAULT_TRANSACTION_FEE);
 unsigned int nTxConfirmTarget = DEFAULT_TX_CONFIRM_TARGET;
