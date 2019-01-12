d216c680dd04092d6c9094821277939fe9bb281f
CryptoCentric <cryptocentric@absolutecoin.net>
2019-01-12 23:23:12 +0000

    Lift the requirement of minimum MN age for regtest/devnet
    Lift the requirement of minimum MN age for regtest/devnet

--- src/masternode.cpp
+++ src/masternode.cpp
@@ -230,12 +230,16 @@ void CMasternode::Check(bool fForce)
         }
     }
 
-    if(lastPing.sigTime - sigTime < MASTERNODE_MIN_MNP_SECONDS) {
-        nActiveState = MASTERNODE_PRE_ENABLED;
-        if(nActiveStatePrev != nActiveState) {
-            LogPrint("masternode", "CMasternode::Check -- Masternode %s is in %s state now\n", vin.prevout.ToStringShort(), GetStateString());
+    // Allow MNs to become ENABLED immediately in regtest/povnet
+    // On mainnet/testnet, we require them to be in PRE_ENABLED state for some time before they get into ENABLED state
+    if (Params().NetworkIDString() != CBaseChainParams::REGTEST && Params().NetworkIDString() != CBaseChainParams::POVNET) {
+        if (lastPing.sigTime - sigTime < MASTERNODE_MIN_MNP_SECONDS) {
+            nActiveState = MASTERNODE_PRE_ENABLED;
+            if (nActiveStatePrev != nActiveState) {
+                LogPrint("masternode", "CMasternode::Check -- Masternode %s is in %s state now\n", vin.prevout.ToStringShort(), GetStateString());
+            }
+            return;
         }
-        return;
     }
 
     nActiveState = MASTERNODE_ENABLED; // OK
