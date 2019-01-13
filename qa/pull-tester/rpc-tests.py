df5cb1be06e1430cf929bc4a44349b583e11e170
CryptoCentric <cryptocentric@absolutecoin.net>
2019-01-13 10:21:47 +0000

    Create signmessagewithprivkey rpc
    7db0ecb Test for signing messages (Andrew Chow)
    f90efbf Create signmessagewithprivkey rpc (Andrew)

--- qa/pull-tester/rpc-tests.py
+++ qa/pull-tester/rpc-tests.py
@@ -146,6 +146,7 @@ testScripts = [
     'abandonconflict.py',
     'p2p-versionbits-warning.py',
     'importprunedfunds.py',
+    'signmessages.py',
 ]
 if ENABLE_ZMQ:
     testScripts.append('zmq_test.py')
