c0b80246db60bfd81b3dfd164bf46084ecce7e63
CryptoCentric <cryptocentric@absolutecoin.net>
2019-01-14 16:30:51 +0000

    Init Move berkeleydb version reporting to wallet
    3e2c946 init: Move berkeleydb version reporting to wallet (Wladimir J. van der Laan)

--- src/init.cpp
+++ src/init.cpp
@@ -43,9 +43,7 @@
 #include "utilmoneystr.h"
 #include "validationinterface.h"
 #ifdef ENABLE_WALLET
-#include "wallet/db.h"
 #include "wallet/wallet.h"
-#include "wallet/walletdb.h"
 #endif
 
 #include "activemasternode.h"
@@ -1179,9 +1177,6 @@ bool AppInit2(boost::thread_group& threadGroup, CScheduler& scheduler)
     if (fPrintToDebugLog)
         OpenDebugLog();
 
-#ifdef ENABLE_WALLET
-    LogPrintf("Using BerkeleyDB version %s\n", DbEnv::version(0, 0, 0));
-#endif
     if (!fLogTimestamps)
         LogPrintf("Startup time: %s\n", DateTimeStrFormat("%Y-%m-%d %H:%M:%S", GetTime()));
     LogPrintf("Default data directory %s\n", GetDefaultDataDir().string());
@@ -1803,7 +1798,6 @@ bool AppInit2(boost::thread_group& threadGroup, CScheduler& scheduler)
 
     if (!connman.Start(scheduler, strNodeError, connOptions))
         return InitError(strNodeError);
-
 
     // ********************************************************* Step 13: finished
 
