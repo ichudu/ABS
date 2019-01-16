0ba3107d09803f77c4b88a0ab83c6ca8bf3a17ce
CryptoCentric <cryptocentric@absolutecoin.net>
2019-01-16 16:45:30 +0000

    RPC add feerate option to fundrawtransaction
    04eaa90 Add more clear interface for CoinControl.h regarding individual feerate (Jonas Schnelli)
    3b35e48 [RPC] add feerate option to fundrawtransaction (Jonas Schnelli)

--- qa/rpc-tests/fundrawtransaction.py
+++ qa/rpc-tests/fundrawtransaction.py
@@ -693,7 +693,25 @@ class RawTransactionsTest(BitcoinTestFramework):
         signedtx = self.nodes[0].signrawtransaction(signedtx["hex"])
         assert(signedtx["complete"])
         self.nodes[0].sendrawtransaction(signedtx["hex"])
+        self.nodes[0].generate(1)
+        self.sync_all()
 
+        #######################
+        # Test feeRate option #
+        #######################
+
+        # Make sure there is exactly one input so coin selection can't skew the result
+        assert_equal(len(self.nodes[3].listunspent(1)), 1)
+
+        inputs = []
+        outputs = {self.nodes[2].getnewaddress() : 1}
+        rawtx = self.nodes[3].createrawtransaction(inputs, outputs)
+        result = self.nodes[3].fundrawtransaction(rawtx) # uses min_relay_tx_fee (set by settxfee)
+        result2 = self.nodes[3].fundrawtransaction(rawtx, {"feeRate": 2*min_relay_tx_fee})
+        result3 = self.nodes[3].fundrawtransaction(rawtx, {"feeRate": 10*min_relay_tx_fee})
+        result_fee_rate = result['fee'] * 1000 / count_bytes(result['hex'])
+        assert_fee_amount(result2['fee'], count_bytes(result2['hex']), 2 * result_fee_rate)
+        assert_fee_amount(result3['fee'], count_bytes(result3['hex']), 10 * result_fee_rate)
 
 if __name__ == '__main__':
     RawTransactionsTest().main()
