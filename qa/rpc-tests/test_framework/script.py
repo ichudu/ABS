b8d22bd6d838cf915adcbe270a928ab2a4e248ba
CryptoCentric <cryptocentric@absolutecoin.net>
2019-02-11 11:23:28 +0000

    Partially cherry-pick tests refactorings found in Bitcoin SegWit PR
    These were mixed into SegWit related PRs and are still needed in Absolute

--- qa/rpc-tests/test_framework/script.py
+++ qa/rpc-tests/test_framework/script.py
@@ -13,8 +13,9 @@
 Functionality to build scripts, as well as SignatureHash().
 """
 
-from .mininode import CTransaction, CTxOut, hash256
+from .mininode import CTransaction, CTxOut, sha256, hash256
 from binascii import hexlify
+import hashlib
 
 import sys
 bchr = chr
@@ -33,6 +34,8 @@ MAX_SCRIPT_ELEMENT_SIZE = 520
 MAX_SCRIPT_OPCODES = 201
 
 OPCODE_NAMES = {}
+def hash160(s):
+    return hashlib.new('ripemd160', sha256(s)).digest()
 
 _opcode_instances = []
 class CScriptOp(int):
