96653851949e3dde148ea3a5b992f1d3d979b127
CryptoCentric <cryptocentric@absolutecoin.net>
2019-02-12 11:17:05 +0000

    Do not set an addr time penalty when a peer adver…
    …tises itself.
    
    6d0ced1 Do not set an addr time penalty when a peer advertises itself. (Gregory Maxwell)

--- src/addrman.cpp
+++ src/addrman.cpp
@@ -251,6 +251,10 @@ bool CAddrMan::Add_(const CAddress& addr, const CNetAddr& source, int64_t nTimeP
     bool fNew = false;
     int nId;
     CAddrInfo* pinfo = Find(addr, &nId);
+    // Do not set a penality for a source's self-announcement
+    if (addr == source) {
+        nTimePenalty = 0;
+    }
 
     if (pinfo) {
         // periodically update nTime
