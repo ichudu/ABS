c6a0a69925c1a40eeebd5e6afb75d161204e451b
CryptoCentric <cryptocentric@absolutecoin.net>
2019-02-12 09:37:24 +0000

    Move static global randomizer seeds into CConnman
    d9ff591 Move static global randomizer seeds into CConnman (Pieter Wuille)

--- src/net.h
+++ src/net.h
@@ -11,6 +11,7 @@
 #include "amount.h"
 #include "bloom.h"
 #include "compat.h"
+#include "hash.h"
 #include "limitedmap.h"
 #include "netaddress.h"
 #include "protocol.h"
@@ -133,7 +134,7 @@ public:
         uint64_t nMaxOutboundTimeframe = 0;
         uint64_t nMaxOutboundLimit = 0;
     };
-    CConnman();
+    CConnman(uint64_t seed0, uint64_t seed1);
     ~CConnman();
     bool Start(CScheduler& scheduler, std::string& strNodeError, Options options);
     void Stop();
@@ -390,6 +391,8 @@ public:
     void SetBestHeight(int height);
     int GetBestHeight() const;
 
+    /** Get a unique deterministic randomizer. */
+    CSipHasher GetDeterministicRandomizer(uint64_t id);
 
     unsigned int GetReceiveFloodSize() const;
 private:
@@ -409,6 +412,7 @@ private:
     void ThreadDNSAddressSeed();
     void ThreadMnbRequestConnections();
 
+    uint64_t CalculateKeyedNetGroup(const CAddress& ad);
     void WakeMessageHandler();
 
     CNode* FindNode(const CNetAddr& ip);
@@ -495,6 +499,8 @@ private:
     std::atomic<int> nBestHeight;
     CClientUIInterface* clientInterface;
 
+    /** SipHasher seeds for deterministic randomness */
+    const uint64_t nSeed0, nSeed1;
     /** flag for waking the message processor. */
     bool fMsgProcWake;
 
@@ -787,7 +793,7 @@ public:
     CAmount lastSentFeeFilter;
     int64_t nextSendTimeFeeFilter;
 
-    CNode(NodeId id, ServiceFlags nLocalServicesIn, int nMyStartingHeightIn, SOCKET hSocketIn, const CAddress &addrIn, const std::string &addrNameIn = "", bool fInboundIn = false, bool fNetworkNodeIn = false);
+    CNode(NodeId id, ServiceFlags nLocalServicesIn, int nMyStartingHeightIn, SOCKET hSocketIn, const CAddress &addrIn, uint64_t nKeyedNetGroupIn, const std::string &addrNameIn = "", bool fInboundIn = false, bool fNetworkNodeIn = false);
     ~CNode();
 
 private:
@@ -797,7 +803,7 @@ private:
     CNode(const CNode&);
     void operator=(const CNode&);
 
-    static uint64_t CalculateKeyedNetGroup(const CAddress& ad);
+
     uint64_t nLocalHostNonce;
     ServiceFlags nLocalServices;
     int nMyStartingHeight;
