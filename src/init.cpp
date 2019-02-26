bytzck/BYTZ

Fix remaining direct uses of mapArgs

src/init.cpp
@@ -907,7 +907,7 @@ void InitParameterInteraction()
         GetBoolArg("-timestampindex", DEFAULT_TIMESTAMPINDEX);
 
     if (fAdditionalIndexes && GetArg("-checklevel", DEFAULT_CHECKLEVEL) < 4) {
-        mapArgs["-checklevel"] = "4";
+        ForceSetArg("-checklevel", "4");
         LogPrintf("%s: parameter interaction: additional indexes -> setting -checklevel=4\n", __func__);
     }
 }
@@ -1008,11 +1008,11 @@ bool AppInitParameterInteraction()
             return InitError(_("Prune mode is incompatible with -txindex."));
     }
 
-    if (mapArgs.count("-devnet")) {
+    if (IsArgSet("-devnet")) {
         // Require setting of ports when running devnet
-        if (GetArg("-listen", DEFAULT_LISTEN) && !mapArgs.count("-port"))
+        if (GetArg("-listen", DEFAULT_LISTEN) && !IsArgSet("-port"))
             return InitError(_("-port must be specified when -devnet and -listen are specified"));
-        if (GetArg("-server", false) && !mapArgs.count("-rpcport"))
+        if (GetArg("-server", false) && !IsArgSet("-rpcport"))
             return InitError(_("-rpcport must be specified when -devnet and -server are specified"));
 
         if (mapMultiArgs.count("-devnet") > 1)

src/qt/splashscreen.cpp
@@ -56,7 +56,7 @@ SplashScreen::SplashScreen(Qt::WindowFlags f, const NetworkStyle *networkStyle) 
         splashScreenPath = ":/images/" + GUIUtil::getThemeName() + "/splash_testnet";
     if(GetBoolArg("-testnet", false))
         splashScreenPath = ":/images/" + GUIUtil::getThemeName() + "/splash_testnet";
-    if(mapArgs.count("-devnet"))
+    if(IsArgSet("-devnet"))
         splashScreenPath = ":/images/" + GUIUtil::getThemeName() + "/splash_testnet";
 
     QString font = QApplication::font().toString();

ichudu/ABS

src/init.cpp
@@ -905,7 +905,7 @@ void InitParameterInteraction()
         GetBoolArg("-timestampindex", DEFAULT_TIMESTAMPINDEX);
 
     if (fAdditionalIndexes && GetArg("-checklevel", DEFAULT_CHECKLEVEL) < 4) {
-        mapArgs["-checklevel"] = "4";
+        ForceSetArg("-checklevel", "4");
         LogPrintf("%s: parameter interaction: additional indexes -> setting -checklevel=4\n", __func__);
     }
 }
@@ -1007,11 +1007,11 @@ bool AppInitParameterInteraction()
 
     }
 
-    if (mapArgs.count("-povnet")) {
-        // Require setting of ports when running PoVNET
-        if (GetArg("-listen", DEFAULT_LISTEN) && !mapArgs.count("-port"))
+    if (IsArgSet("-povnet")) {
+        // Require setting of ports when running povnet
+        if (GetArg("-listen", DEFAULT_LISTEN) && !IsArgSet("-port"))
             return InitError(_("-port must be specified when -povnet and -listen are specified"));
-        if (GetArg("-server", false) && !mapArgs.count("-rpcport"))
+        if (GetArg("-server", false) && !IsArgSet("-rpcport"))
             return InitError(_("-rpcport must be specified when -povnet and -server are specified"));
 
         if (mapMultiArgs.count("-povnet") > 1)

src/qt/splashscreen.cpp
@@ -57,7 +57,7 @@ SplashScreen::SplashScreen(Qt::WindowFlags f, const NetworkStyle *networkStyle) 
         splashScreenPath = ":/images/" + GUIUtil::getThemeName() + "/splash_testnet";
     if(GetBoolArg("-testnet", false))
         splashScreenPath = ":/images/" + GUIUtil::getThemeName() + "/splash_testnet";
-    if(mapArgs.count("-povnet"))
+    if(IsArgSet("-povnet"))
         splashScreenPath = ":/images/" + GUIUtil::getThemeName() + "/splash_testnet";
 
     QString font = QApplication::font().toString();