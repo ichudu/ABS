55b4c6053cbd4dc6ea4b823ff811c9371123b79c
CryptoCentric <cryptocentric@absolutecoin.net>
2019-03-01 09:28:16 +0000

    Pow note add

--- src/pow.cpp
+++ src/pow.cpp
@@ -104,6 +104,7 @@ unsigned int GetNextWorkRequiredBTC(const CBlockIndex* pindexLast, const CBlockH
 
 unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHeader *pblock, const Consensus::Params& params)
 {
+    // Most recent algo first
     if (pindexLast->nHeight + 1 >= params.nPowDGWHeight) {
         return DarkGravityWave(pindexLast, params);
     }
