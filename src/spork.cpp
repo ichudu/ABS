465a2aae962a0cda99bb4d56566b6dc2ad5e7a98
CryptoCentric <cryptocentric@absolutecoin.net>
2019-03-11 18:16:06 +0000

    remove pubkey

--- src/spork.cpp
+++ src/spork.cpp
@@ -60,9 +60,8 @@ void CSporkManager::ProcessSpork(CNode* pfrom, const std::string& strCommand, CD
             LogPrintf("%s new\n", strLogMsg);
         }
 
-        if(!spork.CheckSignature(sporkPubKeyID)) {
-            LOCK(cs_main);
-            LogPrintf("CSporkManager::ProcessSpork -- ERROR: invalid signature\n");
+        if(!spork.CheckSignature()) {
+            LogPrintf("CSporkManager::ProcessSpork -- invalid signature\n");
             Misbehaving(pfrom->GetId(), 100);
             return;
         }
