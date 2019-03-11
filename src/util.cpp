110263258e58314956064a8f734ab5a6aeda6bea
CryptoCentric <cryptocentric@absolutecoin.net>
2019-03-11 15:22:27 +0000

    Add SafeIntVersionToString

--- src/util.cpp
+++ src/util.cpp
@@ -1002,3 +1002,15 @@ std::string IntVersionToString(uint32_t nVersion)
     }
     return boost::join(tokens, ".");
 }
+std::string SafeIntVersionToString(uint32_t nVersion)
+{
+    try
+    {
+        return IntVersionToString(nVersion);
+    }
+    catch(const std::bad_cast&)
+    {
+        return "invalid_version";
+    }
+}
+
