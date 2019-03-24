b443ebd7d26aff6683b5333f5522383aee83b43b
Crypto Centric <cryptocentric@absolutecoin.net>
2019-03-24 21:37:31 +0000

    Chain Set BIP 65 and 66 Height

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -151,9 +151,9 @@ public:
         consensus.nMasternodeMinimumConfirmations = 15;
         consensus.BIP34Height = 0;
         consensus.BIP34Hash = uint256S("0x00000de52875a68d7bf6a5bb5ad1b89fd7df4d67a9603669327949923dc74d7e");
-        consensus.BIP65Height = 500000; // 00000000000076d8fcea02ec0963de4abfd01e771fec0863f960c2c64fe6f357
-        consensus.BIP66Height = 501000; // 00000000000b1fa2dfa312863570e13fae9ca7b5566cb27e55422620b469aefa
-        consensus.DIP0001Height = 750000;
+        consensus.BIP65Height = 390000; 
+        consensus.BIP66Height = 395000; 
+        consensus.DIP0001Height = 450000;
         consensus.powLimit = uint256S("00000fffff000000000000000000000000000000000000000000000000000000");
         consensus.nPowTargetTimespan = 7.5 * 60; // 5 blocks
         consensus.nPowTargetSpacing = 1.5 * 60; // 1.5 minutes
