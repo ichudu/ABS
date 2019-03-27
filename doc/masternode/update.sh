76f45a20d7b331660b9e634d63c3168245fc16c9
madagaga <madagaga@users.noreply.github.com>
2019-03-27 21:56:17 +0100

    update script creation
    This script updates your nodes if installed into $HOME/Absolute
    It also create a symbolic link to /usr/local/bin 
    Old absolute is backuped

---
+++ doc/masternode/update.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+VERSION="absolutecore-0.12.2.5"
+echo 'downloading last release'
+wget https://github.com/absolute-community/absolute/releases/download/v12.2.5/a$
+&& echo "Extracting" \
+&& tar -zxvf absolute_latest.tar.gz \
+&& echo "Backuping"\
+&& mv "$HOME/Absolute/absolute-cli" "$HOME/Absolute/absolute-cli.bkp" \
+&& mv "$HOME/Absolute/absoluted" "$HOME/Absolute/absoluted.bkp" \
+&& echo "updating" \
+&& mv "$VERSION/bin/absolute-cli" "$HOME/Absolute/absolute-cli" \
+&& mv "$VERSION/bin/absoluted" "$HOME/Absolute/absoluted" \
+&& ln -sf $HOME/Absolute/absolute-cli /usr/local/bin/absolute-cli\
+&& ln -sf $HOME/Absolute/absoluted /usr/local/bin/absoluted\
+&& echo "done"
+
