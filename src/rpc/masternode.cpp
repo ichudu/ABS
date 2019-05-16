dashpay/dash-dev-branches

Better "masternode status" for deterministic masternodes

src/rpc/masternode.cpp
@@ -454,15 +454,34 @@ UniValue masternode(const JSONRPCRequest& request)
 
         UniValue mnObj(UniValue::VOBJ);
 
-        mnObj.push_back(Pair("outpoint", activeMasternode.outpoint.ToStringShort()));
-        mnObj.push_back(Pair("service", activeMasternode.service.ToString()));
+        if (deterministicMNList->IsDeterministicMNsSporkActive()) {
+            CProviderTXRegisterMN proTx;
+            if (activeMasternode.proTxHash.IsNull()) {
+                mnObj.push_back(Pair("status", "ProTx hash is unknown. Please specify -masternodeprotx"));
+            } else {
+                mnObj.push_back(Pair("proTxHash", activeMasternode.proTxHash.ToString()));
+                mnObj.push_back(Pair("service", activeMasternode.service.ToString()));
+                CProviderTXRegisterMN proTx;
+                if (deterministicMNList->GetRegisterMN(activeMasternode.proTxHash, proTx)) {
+                    UniValue proTxObj;
+                    proTx.ToJson(proTxObj);
+                    mnObj.push_back(Pair("proTx", proTxObj));
+                }
+                mnObj.push_back(Pair("state", activeMasternodeManager->GetStateString()));
+                mnObj.push_back(Pair("status", activeMasternodeManager->GetStatus()));
+            }
 
-        CMasternode mn;
-        if(mnodeman.Get(activeMasternode.outpoint, mn)) {
-            mnObj.push_back(Pair("payee", CBitcoinAddress(mn.pubKeyIDCollateralAddress).ToString()));
+        } else {
+            mnObj.push_back(Pair("outpoint", activeMasternode.outpoint.ToStringShort()));
+            mnObj.push_back(Pair("service", activeMasternode.service.ToString()));
+
+            CMasternode mn;
+            if (mnodeman.Get(activeMasternode.outpoint, mn)) {
+                mnObj.push_back(Pair("payee", CBitcoinAddress(mn.pubKeyIDCollateralAddress).ToString()));
+            }
+
+            mnObj.push_back(Pair("status", legacyActiveMasternodeManager.GetStatus()));
         }
-
-        mnObj.push_back(Pair("status", legacyActiveMasternodeManager.GetStatus()));
         return mnObj;
     }
 

ichudu/ABS

src/rpc/masternode.cpp
@@ -627,15 +627,29 @@ UniValue masternode_status(const JSONRPCRequest& request)
 
     UniValue mnObj(UniValue::VOBJ);
 
+    // keep compatibility with legacy status for now (might get deprecated/removed later)
     mnObj.push_back(Pair("outpoint", activeMasternodeInfo.outpoint.ToStringShort()));
     mnObj.push_back(Pair("service", activeMasternodeInfo.service.ToString()));
 
-    CMasternode mn;
-    if(mnodeman.Get(activeMasternodeInfo.outpoint, mn)) {
-        mnObj.push_back(Pair("payee", CBitcoinAddress(mn.keyIDCollateralAddress).ToString()));
-    }
+    if (deterministicMNManager->IsDeterministicMNsSporkActive()) {
+        auto dmn = activeMasternodeManager->GetDMN();
+        if (dmn) {
+            mnObj.push_back(Pair("proTxHash", dmn->proTxHash.ToString()));
+            mnObj.push_back(Pair("collateralIndex", (int)dmn->nCollateralIndex));
+            UniValue stateObj;
+            dmn->pdmnState->ToJson(stateObj);
+            mnObj.push_back(Pair("dmnState", stateObj));
+        }
+        mnObj.push_back(Pair("state", activeMasternodeManager->GetStateString()));
+        mnObj.push_back(Pair("status", activeMasternodeManager->GetStatus()));
+    } else {
+        CMasternode mn;
+        if(mnodeman.Get(activeMasternodeInfo.outpoint, mn)) {
+            mnObj.push_back(Pair("payee", CBitcoinAddress(mn.keyIDCollateralAddress).ToString()));
+        }
 
-    mnObj.push_back(Pair("status", legacyActiveMasternodeManager.GetStatus()));
+        mnObj.push_back(Pair("status", legacyActiveMasternodeManager.GetStatus()));
+    }
     return mnObj;
 }
 