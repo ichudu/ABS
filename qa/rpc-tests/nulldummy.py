d8ba39674314b4e4f64df23b7e9ae1118ca7b17b
CryptoCentric <cryptocentric@absolutecoin.net>
2019-05-16 15:39:45 +0100

    Fix nulldummy tests by creating correct AIP4 coinbase transactions

--- qa/rpc-tests/nulldummy.py
+++ qa/rpc-tests/nulldummy.py
@@ -113,7 +113,8 @@ class NULLDUMMYTest(BitcoinTestFramework):
 
 
     def block_submit(self, node, txs, accept = False):
-        block = create_block(self.tip, create_coinbase(self.lastblockheight + 1), self.lastblocktime + 1)
+        aip4_activated = self.lastblockheight + 1 >= 432
+        block = create_block(self.tip, create_coinbase(self.lastblockheight + 1, dip4_activated=aip4_activated), self.lastblocktime + 1)
         block.nVersion = 4
         for tx in txs:
             tx.rehash()
