47f63a77c892cc697462c6a52513f9e68108ab4e
CryptoCentric <cryptocentric@absolutecoin.net>
2019-05-20 08:55:31 +0100

    Bump PROTOCOL_VERSION
    Bump PROTOCOL_VERSION to 70211,
    
    MIN protocols to 70210

--- src/instantx.h
+++ src/instantx.h
@@ -27,7 +27,7 @@ extern CInstantSend instantsend;
     (1000/2900.0)**5 = 0.004875397277841433
 */
 
-static const int MIN_INSTANTSEND_PROTO_VERSION      = 70209;
+static const int MIN_INSTANTSEND_PROTO_VERSION      = 70211;
 
 /// For how long we are going to accept votes/locks
 /// after we saw the first one for a specific transaction
