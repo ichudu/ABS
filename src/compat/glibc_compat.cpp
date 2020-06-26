5e3bcd5f1861aad2c4b124d984aabd46e41cc365
Crypto Centric <cryptocentric@absolutecoin.net>
2020-06-26 17:40:32 +0100

    Push GLIBC_2.27 check

--- src/compat/glibc_compat.cpp
+++ src/compat/glibc_compat.cpp
@@ -55,7 +55,7 @@ __asm(".symver log2f_old,log2f@GLIBC_2.4");
 #elif defined(__aarch64__)
 __asm(".symver log2f_old,log2f@GLIBC_2.17");
 #elif defined(__riscv)
-__asm(".symver log2f_old,log2f@GLIBC_2.17");
+__asm(".symver log2f_old,log2f@GLIBC_2.27");
 #endif
 extern "C" float __wrap_log2f(float x)
 {
