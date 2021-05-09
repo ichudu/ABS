8013de065bfd00788a090e464b3c45c60450980a
CryptoCentric <cryptocentric@absolutecoin.net>
2021-05-09 17:47:36 +0100

    Contrib: tidy databases

--- contrib/tidy_datadir.sh
+++ contrib/tidy_datadir.sh
@@ -12,7 +12,10 @@ else
 fi
 
 LEVEL=0
-if [ -f wallet.dat -a -f peers.dat -a -f chainstate/CURRENT -a -f blocks/index/CURRENT -a -f blocks/blk00000.dat ]; then LEVEL=1; fi
+if [ -f wallet.dat -a -f addr.dat -a -f blkindex.dat -a -f blk0001.dat ]; then LEVEL=1; fi
+if [ -f wallet.dat -a -f peers.dat -a -f blkindex.dat -a -f blk0001.dat ]; then LEVEL=2; fi
+if [ -f wallet.dat -a -f peers.dat -a -f coins/CURRENT -a -f blktree/CURRENT -a -f blocks/blk00000.dat ]; then LEVEL=3; fi
+if [ -f wallet.dat -a -f peers.dat -a -f chainstate/CURRENT -a -f blocks/index/CURRENT -a -f blocks/blk00000.dat ]; then LEVEL=4; fi
 
 case $LEVEL in
   0)
@@ -27,9 +30,9 @@ esac
 FILES=""
 DIRS=""
 
-if [ $LEVEL -ge 1 ]; then FILES=$(echo $FILES blk????.dat blkindex.dat); fi
-if [ $LEVEL -ge 1 ]; then FILES=$(echo $FILES addr.dat); fi
-if [ $LEVEL -ge 1 ]; then DIRS=$(echo $DIRS coins blktree); fi
+if [ $LEVEL -ge 3 ]; then FILES=$(echo $FILES blk????.dat blkindex.dat); fi
+if [ $LEVEL -ge 2 ]; then FILES=$(echo $FILES addr.dat); fi
+if [ $LEVEL -ge 4 ]; then DIRS=$(echo $DIRS coins blktree); fi
 
 for FILE in $FILES; do
   if [ -f $FILE ]; then
